28ed1297c5a939a35074c8f0f71eaeceee7f704b
kushvinth <kushvinth.m@gmail.com>
2026-01-21 21:28:53 +0530

    Feat(ingession): shutil to move downloads to data as .wav

--- app/ingession.py
+++ app/ingession.py
@@ -3,13 +3,14 @@ import yt_dlp
 import ffmpeg
 import os
 from fastapi import FastAPI
+import shutil
 
 URL = "https://itunes.apple.com/search"
 
-app = FastAPI(title="Ingession API")
+# app = FastAPI(title="Ingession API")
 
 
-@app.get("/download")
+# @app.get("/download")
 def download_mp3(NAME: str):
     if NAME:
         params = {
@@ -27,20 +28,28 @@ def download_mp3(NAME: str):
 
         ytd_params = {
             "format": "bestaudio/best",
-            "outtmpl": "./data/%(title)s.%(ext)s",
+            "outtmpl": "./download/%(title)s.%(ext)s",
         }
         with yt_dlp.YoutubeDL(ytd_params) as ydl:  # type: ignore
             for i in data["results"]:
                 query = f"{i['artistName']} - {i['trackName']} official audio"
                 ydl.download([f"ytsearch1: {query}"])
 
-        for i in os.listdir("./data"):
-            file_path = os.path.join("./data", i)
+        for i in os.listdir("./download"):
+            if i.startswith("."):  ## FIXs the .DS_Store ERROR
+                continue
+            file_path = os.path.join("./download", i)
             # print("-" * 30)
             # print(i)
-            if not i.endswith(".mp3"):
+            if i.endswith((".mp3", ".webm", ".m4a", ".aac")):
                 print("-" * 30)
                 ffmpeg.input(file_path).output(
                     os.path.splitext(file_path)[0] + ".wav", audio_bitrate="192k"
                 ).run(overwrite_output=True)
-                os.remove("./data/" + i)
+                os.remove("./download/" + i)
+
+        for filename in os.listdir("./download"):
+            print(f"File Name with .wav {filename}")
+            if filename.endswith(".wav"):
+                shutil.move(os.path.join("./download", filename), "./data/")
+                print("Moved to data")
